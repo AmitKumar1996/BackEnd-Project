28bb59168bba6572bbef5d13eb0d933683e510bd
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-01-06 22:11:04 +0530

    Random Test remove

---
+++ src/test/java/dev/amit/ProductService/RandomTest.java
@@ -0,0 +1,18 @@
+package dev.amit.ProductService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Random;
+
+@SpringBootTest
+public class RandomTest {
+    @Test
+   void testLessThan3(){
+//        Random random= new Random();
+//        int number= random.nextInt();
+//
+//        assert(number < 1000000000);
+
+    }
+}
