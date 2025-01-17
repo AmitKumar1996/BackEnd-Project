0fab8cdb3dc26b97bd2ee36b22c7a8bf63479e0a
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-01-17 22:08:08 +0530

    Removed empty Microservice directory

--- src/main/java/dev/amit/ProductService/repository/CustomQuries.java
+++
@@ -1,5 +0,0 @@
-package dev.amit.ProductService.repository;
-
-public interface CustomQuries {
-    String FIND_ALL_PRODUCTS_BY_TITLE = "select * from product join product_orders on product.id = product_orders.product_id where title = :amit ";
-}
