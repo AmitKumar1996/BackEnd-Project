6edf1f0f63b8a4c8c1db965fa6945dd8ca049ad6
AmitKumar1996 <kumaramit1996.1@gmail.com>
2025-01-17 21:39:08 +0530

    Added userMicroservice from userservicetestfinal project

---
+++ userMicroservice/src/main/java/dev/amit/userservicetestfinal/UserservicetestfinalApplication.java
@@ -0,0 +1,17 @@
+package dev.amit.userservicetestfinal;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserservicetestfinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserservicetestfinalApplication.class, args);
+
+
+		System.out.println("Jay Shri Ram !");
+	}
+
+}
